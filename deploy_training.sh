729dd55fea67cabd72d503276b2300c73dbce8cc
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2026-01-14 16:45:04 -0600

    Update deploy_training.sh

--- deploy_training.sh
+++ deploy_training.sh
@@ -4,16 +4,16 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-echo -e "${YELLOW}🚀 INICIANDO DESPLIEGUE (Smability AI)${NC}"
+echo -e "\${YELLOW}🚀 INICIANDO DESPLIEGUE (Smability AI)\${NC}"
 
-# Git Workflow - Usamos "git add ." para que no se le escape nada
-echo -e "${YELLOW}📦 Empaquetando cambios... (git add .)${NC}"
+# Git Workflow
+echo -e "\${YELLOW}📦 Empaquetando cambios... (git add .)\${NC}"
 git add .
 
-echo -e "${YELLOW}📝 Mensaje del commit: 'Force Update Buildspec'${NC}"
-git commit -m "Force Update Buildspec and Dataset Logic"
+echo -e "\${YELLOW}📝 Mensaje del commit: 'Ready for Grand Slam Training'\${NC}"
+git commit -m "Grand Slam Config: S3 Source + V5 Script"
 
-echo -e "${YELLOW}⬆️ Subiendo a GitHub...${NC}"
+echo -e "\${YELLOW}⬆️ Subiendo a GitHub...\${NC}"
 git push origin main
 
-echo -e "${GREEN}✅ Código enviado. Revisa CodeBuild.${NC}"
+echo -e "\${GREEN}✅ Código enviado. ¡Corre a ver CodeBuild!\${NC}"
